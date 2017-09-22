9e19f72a4642a286c5eb89eccd1475df4bf3c4b5
dmitrij <dmitrij@Dmitrij-PC>
2017-09-22 09:34:12 +0300

    Нормальный Cmake

---
+++ build/CMakeFiles/bigmistake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bigmistake.dir/source/main.cpp.o"
+  "bigmistake.pdb"
+  "bigmistake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bigmistake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
